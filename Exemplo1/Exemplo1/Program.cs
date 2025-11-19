e5d8f919b8978c433737fa6c6f0cda8bd3466f64
Daniel-Messias007 <daniel.dullo@etec.sp.gov.br>
2025-11-18 23:31:20 -0300

    Add files via upload

---
+++ Exemplo1/Exemplo1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11, n12, n13, n14;
+            
+            Console.Write("Digite o 1º nome: "); n1 = Console.ReadLine();
+            Console.Write("Digite o 2º nome: "); n2 = Console.ReadLine();
+            Console.Write("Digite o 3º nome: "); n3 = Console.ReadLine();
+            Console.Write("Digite o 4º nome: "); n4 = Console.ReadLine();
+            Console.Write("Digite o 5º nome: "); n5 = Console.ReadLine();
+            Console.Write("Digite o 6º nome: "); n6 = Console.ReadLine();
+            Console.Write("Digite o 7º nome: "); n7 = Console.ReadLine();
+            Console.Write("Digite o 8º nome: "); n8 = Console.ReadLine();
+            Console.Write("Digite o 9º nome: "); n9 = Console.ReadLine();
+            Console.Write("Digite o 10º nome: "); n10 = Console.ReadLine();
+            Console.Write("Digite o 11º nome: "); n11 = Console.ReadLine();
+            Console.Write("Digite o 12º nome: "); n12 = Console.ReadLine();
+            Console.Write("Digite o 13º nome: "); n13 = Console.ReadLine();
+            Console.Write("Digite o 14º nome: "); n14 = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("1º nome: " + n1);
+            Console.WriteLine("2º nome: " + n2);
+            Console.WriteLine("3º nome: " + n3);
+            Console.WriteLine("4º nome: " + n4);
+            Console.WriteLine("5º nome: " + n5);
+            Console.WriteLine("6º nome: " + n6);
+            Console.WriteLine("7º nome: " + n7);
+            Console.WriteLine("8º nome: " + n8);
+            Console.WriteLine("9º nome: " + n9);
+            Console.WriteLine("10º nome: " + n10);
+            Console.WriteLine("11º nome: " + n11);
+            Console.WriteLine("12º nome: " + n12);
+            Console.WriteLine("13º nome: " + n13);
+            Console.WriteLine("14º nome: " + n14);
+
+            Console.ReadKey();
+        }
+    }
+}
